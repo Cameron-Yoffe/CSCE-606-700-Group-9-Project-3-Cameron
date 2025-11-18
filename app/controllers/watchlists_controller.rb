ab7c5473edd6d6936681af8fca3a1f5f70f85d2a
Omarkjee <62869027+Omarkjee@users.noreply.github.com>
2025-11-18 23:46:51 +0000

    US-103: Add Movie to My Library

---
+++ app/controllers/watchlists_controller.rb
@@ -0,0 +1,67 @@
+class WatchlistsController < ApplicationController
+  before_action :require_login
+
+  def create
+    tmdb_id = params[:tmdb_id] || params.dig(:movie, :tmdb_id)
+    title = params[:title] || params.dig(:movie, :title)
+
+    if tmdb_id.blank?
+      respond_to do |format|
+        format.html { redirect_back fallback_location: movies_path, alert: "Missing movie id" }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash", locals: { alert: "Missing movie id" }) }
+      end
+      return
+    end
+
+    movie = Movie.find_or_create_by(tmdb_id: tmdb_id.to_i) do |m|
+      m.title = title.presence || "Untitled"
+    end
+
+    watchlist = current_user.watchlists.find_or_create_by(movie: movie)
+    @watchlist = watchlist
+
+    respond_to do |format|
+      format.html do
+        if watchlist.persisted?
+          redirect_back fallback_location: movies_path, notice: "Added to your library"
+        else
+          redirect_back fallback_location: movies_path, alert: "Could not add to library"
+        end
+      end
+      format.turbo_stream do
+        if watchlist.persisted?
+          render :create
+        else
+          render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash", locals: { alert: "Could not add to library" })
+        end
+      end
+    end
+  end
+
+  def destroy
+    watchlist = current_user.watchlists.find_by(id: params[:id])
+
+    unless watchlist
+      respond_to do |format|
+        format.html { redirect_back fallback_location: dashboard_path, alert: "Item not found in your library" }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash", locals: { alert: "Item not found in your library" }) }
+      end
+      return
+    end
+
+    movie = watchlist.movie
+    wl_id = watchlist.id
+    watchlist.destroy
+
+    respond_to do |format|
+      format.html { redirect_back fallback_location: dashboard_path, notice: "Removed from your library" }
+      format.turbo_stream { render :destroy, locals: { movie: movie, watchlist_id: wl_id } }
+    end
+  end
+
+  private
+
+  def require_login
+    redirect_to sign_up_path, alert: "You must be logged in" unless logged_in?
+  end
+end
