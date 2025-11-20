2f7aff8908757ee7392baf37ae2076422be2164a
Bryan Ramos <bmr.ramos10@gmail.com>
2025-11-19 21:56:07 -0700

    US-106: Rate a Movie

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,64 @@
+class RatingsController < ApplicationController
+  before_action :require_login
+  before_action :set_movie, only: [ :create, :update ]
+  before_action :set_rating, only: [ :update ]
+
+  def create
+    @rating = current_user.ratings.build(rating_params)
+    @rating.movie_id = @movie.id
+
+    if @rating.save
+      respond_to do |format|
+        format.json { render json: { success: true, rating: @rating }, status: :created }
+        format.html { redirect_to movie_path(@movie.tmdb_id), notice: "Rating saved successfully." }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: { success: false, errors: @rating.errors.full_messages }, status: :unprocessable_entity }
+        format.html { redirect_to movie_path(@movie.tmdb_id), alert: "Error saving rating." }
+      end
+    end
+  end
+
+  def update
+    if @rating.update(rating_params)
+      respond_to do |format|
+        format.json { render json: { success: true, rating: @rating } }
+        format.html { redirect_to movie_path(@movie.tmdb_id), notice: "Rating updated successfully." }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: { success: false, errors: @rating.errors.full_messages }, status: :unprocessable_entity }
+        format.html { redirect_to movie_path(@movie.tmdb_id), alert: "Error updating rating." }
+      end
+    end
+  end
+
+  private
+
+  def set_movie
+    if params[:movie_id].present?
+      @movie = Movie.find(params[:movie_id])
+    elsif params[:rating]&.fetch(:movie_id, nil).present?
+      @movie = Movie.find(params[:rating][:movie_id])
+    elsif params[:id].present?
+      # For update action, find the rating first then get the movie
+      @rating = current_user.ratings.find(params[:id])
+      @movie = @rating.movie
+    end
+  end
+
+  def set_rating
+    if @rating.nil?
+      @rating = current_user.ratings.find_by!(movie_id: @movie.id)
+    end
+  end
+
+  def rating_params
+    params.require(:rating).permit(:value, :review, :movie_id)
+  end
+
+  def require_login
+    redirect_to sign_in_path unless logged_in?
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,22 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Configure SimpleCov for code coverage
+require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::CoberturaFormatter
+]
+
+SimpleCov.start('rails') do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+  add_filter '/config/'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
