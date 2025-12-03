6045bb183ea1a784ac05e2aa738a30d50d698f52
Bryan Ramos <bmr.ramos10@gmail.com>
2025-12-03 00:23:22 -0700

    app/helpers/movies_helper.rb fix

--- spec/helpers/movies_helper_spec.rb
+++ spec/helpers/movies_helper_spec.rb
@@ -30,4 +30,22 @@ RSpec.describe MoviesHelper, type: :helper do
       expect(helper.director_name({})).to eq("N/A")
     end
   end
+
+  describe "#release_year" do
+    it "returns the year from a valid release_date" do
+      movie = { "release_date" => "2010-07-16" }
+      expect(helper.release_year(movie)).to eq(2010)
+    end
+
+    it "returns N/A when release_date is blank" do
+      expect(helper.release_year({ "release_date" => "" })).to eq("N/A")
+      expect(helper.release_year({ "release_date" => nil })).to eq("N/A")
+      expect(helper.release_year({})).to eq("N/A")
+    end
+
+    it "returns N/A when release_date is invalid" do
+      movie = { "release_date" => "invalid-date" }
+      expect(helper.release_year(movie)).to eq("N/A")
+    end
+  end
 end
